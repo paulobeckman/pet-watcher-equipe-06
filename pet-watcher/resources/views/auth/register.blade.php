e2aaea8461d7f03f03242e579f68a2af138c4d90
sevirotimo <jncosta201@gmail.com>
2021-08-10 12:44:57 -0300

    rigistro form

--- pet-watcher/resources/views/auth/register.blade.php
+++ pet-watcher/resources/views/auth/register.blade.php
@@ -5,8 +5,8 @@
 <form action="{{ route('register') }}" method="post">
     @csrf
 
-    <input id="name" type="text"  name="name" value="{{ old('name') }}"  autofocus>
-    <input id="email" type="email"  name="email" value="{{ old('email') }}" >
+    <input id="name" type="text"  name="name" >
+    <input id="email" type="email"  name="email" >
     <input id="password" type="password" name="password" >
     <input id="password-confirm" type="password" name="password_confirmation" >
     <button type="submit">register</button>
