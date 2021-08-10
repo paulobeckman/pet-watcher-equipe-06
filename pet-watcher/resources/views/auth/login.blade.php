809b9c395eb0431295200964df9ee342d0027838
Daniel <pdaniel.stm@gmail.com>
2021-08-10 19:02:29 -0300

    Views de login, create usuario e create especie

--- pet-watcher/resources/views/auth/login.blade.php
+++ pet-watcher/resources/views/auth/login.blade.php
@@ -2,14 +2,25 @@
 
 @section('content')
 
-
+<div id="page-login">
+    <main>
+        <nav class = "navbar">Pet Watcher</nav>
 
 <form action=" {{route('login')}} "method="post"  >
     @csrf
-    <input  type="email" name="email" id="email" placeholder="login">
+    
+    <div class="input-block">
+        <input  type="email" name="email" id="email" placeholder="login">
+    </div>
+    
+    <div class="input-block">
     <input  type="password" name="password" id="password" placeholder="senha">
+    </div>
 
-    <input type="submit" value="acao">
+    <input type="submit" class= "primary-button" value="Entrar">
 </form>
+    </main>
+</div>
+
 
 @stop
--- pet-watcher/resources/views/especies/create.blade.php
+++ pet-watcher/resources/views/especies/create.blade.php
@@ -1,10 +1,34 @@
 @extends('layout')
 
 @section('content')
+<div id = "page-create-especie">
+    <main>
+        <nav id = "menu">
+            <ul>
+                <li><a href = "#">Cadastro</a></li>
+                <li><a href = "#">Espécie</a></li>
+                <li><a href = "#">Consulta</a></li>
+                <li><a href = "#">Licença</a></li>
+                <li><a href = "#">Alterar Senha</a></li>
+                <li><a href = "#">Logout</a></li>
+            </ul>
+        </nav>
+    </main>
+</div>
     <form action="{{action('EspecieController@store')}}" method="post">
         @csrf
-        <input type="text" name="nome_cientifico" id="nome_cientifico">
-        <input type="text" name="nome_popular" id="nome_popular">
-        <input type="submit" value="acao">
+        <legend>Cadastrar Espécie</legend>
+        <br>    
+        <div class="input-block">
+            <legend>Nome Científico</legend>
+            <input type="text" name="nome_cientifico" id="nome_cientifico">
+        </div>
+
+        <div class="input-block">
+            <legend>Nome Popular</legend>
+            <input type="text" name="nome_popular" id="nome_popular">
+        </div>        
+        
+        <input type="submit" class = "primary-button" value="Cadastrar">
     </form>
 @stop
