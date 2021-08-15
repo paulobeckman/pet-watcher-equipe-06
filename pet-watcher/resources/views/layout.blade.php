e45f5b6b058c354267bb93bd8369b3405ad4d221
sevirotimo <jncosta201@gmail.com>
2021-08-15 01:18:41 -0300

    acrescimo  da gestão de credenciadas

--- pet-watcher/resources/views/layout.blade.php
+++ pet-watcher/resources/views/layout.blade.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    <link href ="{{url('assets/login.css')}}" rel = "stylesheet">
-    <link href ="{{url('assets/create-especie.css')}}" rel = "stylesheet">
+    <link href="{{url('assets/login.css')}}" rel="stylesheet">
+    <link href="{{url('assets/create-especie.css')}}" rel="stylesheet">
 </head>
+
 <body>
 
     <div>
         @if(auth()->guest())
-            <a href=" {{route('login')}} "> login</a>
+        <a href=" {{route('login')}} "> login</a>
         @endif
 
         @if(auth()->check())
-            {{method_field('get')}}
+        {{method_field('get')}}
+        <div >
             <a href="{{ route('logout') }} "> logout</a>
+            <a href="{{ route('reset')}}"> troca senha</a>
+            <a href="{{ route('credenciada.index')}}">gestão de credenciadas</a>
+        </div>
+
         @endif
 
-        <a href="{{ route('register') }}"> cadastro</a>
+        <a href="#"> cadastro</a>
     </div>
 
     @yield('content')
 </body>
+
 </html>
