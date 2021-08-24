c1c0bb464195338019d38d914ff23866bcea9325
Daniel <pdaniel.stm@gmail.com>
2021-08-24 11:13:50 -0300

    mudança em credenciada show.blade [view]

--- pet-watcher/resources/views/credenciada/show.blade.php
+++ pet-watcher/resources/views/credenciada/show.blade.php
@@ -23,6 +23,8 @@
             <a class = "btn btn-primary" href=" {{route('credenciada.edit',$credenciada->id)}}">Editar {{$credenciada->razao_social}}</a>
             <a style = "margin: 1rem"class = "btn btn-secondary" href=" {{route('empregado.create',['id' => $credenciada->id])}}">Cadastrar funcionários</a>
             <a class = "btn btn-info" href=" {{route('licenca.create',['id' => $credenciada->id])}}">Cadastrar licença para {{$credenciada->razao_social}}</a>
+            <button><a href=" {{route('animal.index',['id' => $credenciada->id])}}">Gerenciar animais</a></button>
+            <button><a href=" {{route ('proprietario.index',['id'=>$credenciada->id])}}">Gerenciamento de proprietario de animais</a></button>
         </tr>
 
         @foreach($licencas as $licenca)
@@ -31,8 +33,8 @@
             <td> {{$licenca->data_vencimento}} </td>
             <td> {{$licenca->data_licenciamento}} </td>
             @if($licenca->data_revogacao === null)
-                <td> licença ativa </td>
-            <button><a href=" {{route('licenca.revogar',['id' => $credenciada->id, 'idlicenca' => $licenca->id])}}">revogar licenca</a></button>
+                <td> Licença Ativa </td>
+            <button><a href=" {{route('licenca.revogar',['id' => $credenciada->id, 'idlicenca' => $licenca->id])}}">Revogar Licenca</a></button>
 
             @endif
             @if($licenca->data_revogacao != null)
@@ -50,3 +52,6 @@
 
 </table>
 @stop
+
+
+
