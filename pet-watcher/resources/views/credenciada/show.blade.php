a376ff88b08fe6e25f2dba03eec90b0a732bfb6a
sevirotimo <jncosta201@gmail.com>
2021-08-26 14:45:00 -0300

    correção no index do animal

--- pet-watcher/resources/views/credenciada/show.blade.php
+++ pet-watcher/resources/views/credenciada/show.blade.php
@@ -4,7 +4,6 @@
 @section('content')
 
 <div class="card">
-   
     <div class="card-body">
         <h5 class="card-title"> {{$credenciada->razao_social}}    </h5>
         <p class="card-text">Localizada em: {{$credenciada->endereco  }}</p>
@@ -13,7 +12,7 @@
 
 
             <tbody>
-        
+
                 @foreach($licencas as $licenca)
                 <thead>
                     <th>Id da Licença</th>
@@ -28,16 +27,16 @@
                     <td> {{$licenca->data_licenciamento}} </td>
                     @if($licenca->data_revogacao === null)
                         <td> Licença Ativa
-                            <a style = "margin: .4rem"class = "btn-outline-danger" href=" {{route('licenca.revogar',['id' => $credenciada->id, 'idlicenca' => $licenca->id])}}">Revogar Licenca</a> 
-  
+                            <a style = "margin: .4rem"class = "btn-outline-danger" href=" {{route('licenca.revogar',['id' => $credenciada->id, 'idlicenca' => $licenca->id])}}">Revogar Licenca</a>
+
                     @endif
                     @if($licenca->data_revogacao != null)
                         <td> licença revogada na data {{$licenca->data_revogacao}} </td>
                     @endif
-        
+
                 </tr>
                 @endforeach
-                           
+
 
         </table>
         <br>
@@ -54,8 +53,10 @@
             <a class = "btn btn-primary" href=" {{route('credenciada.edit',$credenciada->id)}}">Editar {{$credenciada->razao_social}}</a>
             <a style = "margin: 1rem"class = "btn btn-secondary" href=" {{route('empregado.create',['id' => $credenciada->id])}}">Cadastrar funcionários</a>
             <a class = "btn btn-info" href=" {{route('licenca.create',['id' => $credenciada->id])}}">Cadastrar licença para {{$credenciada->razao_social}}</a><br><br>
-            <a class="btn btn-success" shref=" {{route('animal.index',['id' => $credenciada->id])}}">Gerenciar animais</a>
+            <a class="btn btn-success" href="{{ route('animal.index',['id' => $credenciada->id]) }}">Gerenciar animais</a>
             <a class="btn btn-success" href=" {{route ('proprietario.index',['id'=>$credenciada->id])}}">Gerenciamento de proprietário de animais</a>
+            <a class="btn btn-success" href=" {{action ('EspecieController@index')}}">Gerenciamento de especies</a>
+            <br>
             <td> {{$empregado->nome_completo}} </td>
             <td> {{$empregado->cpf}} </td>
         </tr>
